cf4cb619aeee404b9d2874d38d7af35ede4c6f24
Adam Boislevant Fleury <boislevant@cy-tech.fr>
2024-11-25 09:21:37 +0100

    camera switcher bis

---
+++ Projet_Appartement/Assets/Scripts/SwitchCameras.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] private Transform cameras;
+    public KeyCode switchKey = KeyCode.C; // Touche pour changer de caméra
+    private int currentCameraIndex = 0;
+
+    void Start()
+    {
+        // Désactiver toutes les caméras sauf la première
+        for (int i = 0; i < cameras.childCount; i++)
+        {
+            cameras.GetChild(i).gameObject.SetActive(i == currentCameraIndex);
+            
+        }
+    }
+
+    void Update()
+    {
+        // Vérifier si la touche de changement de caméra est pressée
+        if (Input.GetKeyDown(switchKey))
+        {
+            SwitchCamera();
+        }
+    }
+
+    void SwitchCamera()
+    {
+        // Désactiver la caméra actuelle
+        cameras.GetChild(currentCameraIndex).gameObject.SetActive(false);
+
+        // Passer à la caméra suivante
+        currentCameraIndex = (currentCameraIndex + 1) % cameras.childCount;
+
+        // Activer la nouvelle caméra
+        cameras.GetChild(currentCameraIndex).gameObject.SetActive(true);
+    }
+}
